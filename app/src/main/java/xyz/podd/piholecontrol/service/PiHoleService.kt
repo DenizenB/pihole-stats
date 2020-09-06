2edc5a1aaada8a17bee6f4371089156f445b9063
SomewhatDisoriented <github@podd.xyz>
2020-09-06 19:55:55 +0200

    Add daily summary endpoint

--- app/src/main/java/xyz/podd/piholecontrol/service/PiHoleService.kt
+++ app/src/main/java/xyz/podd/piholecontrol/service/PiHoleService.kt
@@ -11,12 +11,28 @@ interface PiHoleService {
 	@GET("api.php?status")
 	suspend fun getStatus(): Response<Status>
 
+	@GET("api.php?summary")
+	suspend fun getSummary(@Query("auth") authToken: String): Response<Summary>
+
 	@GET("api.php?topItems")
 	suspend fun getTopItems(@Query("auth") authToken: String): Response<TopItems>
 }
 
 @Serializable
 data class Status(val status: String) {
+	val enabled
+		get() = status == "enabled"
+}
+
+@Serializable
+data class Summary(
+	val status: String,
+	@SerialName("domains_being_blocked") val domainsInBlocklist: String,
+	@SerialName("dns_queries_today") val queriesToday: String,
+	@SerialName("ads_blocked_today") val blockedToday: String,
+	@SerialName("ads_percentage_today") val blockedTodayPercentage: String,
+	@SerialName("unique_domains") val uniqueDomains: String
+) {
 	val enabled
 		get() = status == "enabled"
 }

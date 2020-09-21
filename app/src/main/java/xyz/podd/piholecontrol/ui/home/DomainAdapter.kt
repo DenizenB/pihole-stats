3329d5f9fc6687d1d3a5646306441bc6c01fb5cd
DenizenB <github@podd.xyz>
2020-09-21 15:01:55 +0200

    Trim overflowing domains at the start

--- app/src/main/java/xyz/podd/piholecontrol/ui/home/DomainAdapter.kt
+++ app/src/main/java/xyz/podd/piholecontrol/ui/home/DomainAdapter.kt
@@ -9,16 +9,11 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import xyz.podd.piholecontrol.R
 
-class DomainAdapter(blockedDomains: Boolean): ListAdapter<Map.Entry<String, Int>, DomainViewHolder>(DiffUtilCallback) {
-    private val textAreaIdForValue = when (blockedDomains) {
-        true -> R.id.text_blocked
-        else -> R.id.text_queries
-    }
-
+class DomainAdapter(private val blockedDomains: Boolean): ListAdapter<Map.Entry<String, Int>, DomainViewHolder>(DiffUtilCallback) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DomainViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_domain, parent, false)
 
-        return DomainViewHolder(view, textAreaIdForValue)
+        return DomainViewHolder(view, blockedDomains)
     }
 
     override fun onBindViewHolder(holder: DomainViewHolder, position: Int) {
@@ -36,16 +31,19 @@ class DomainAdapter(blockedDomains: Boolean): ListAdapter<Map.Entry<String, Int>
     }
 }
 
-class DomainViewHolder(view: View, textAreaIdForValue: Int): RecyclerView.ViewHolder(view) {
+class DomainViewHolder(view: View, blockedDomain: Boolean): RecyclerView.ViewHolder(view) {
     private val textDomain: TextView = view.findViewById(R.id.text_name)
-    private val textValue: TextView = view.findViewById(textAreaIdForValue)
+    private val textCount: TextView = view.findViewById(R.id.text_count)
 
     init {
-        textValue.visibility = View.VISIBLE
+        if (blockedDomain) {
+            val color = textCount.context.getColor(android.R.color.holo_red_light)
+            textCount.setTextColor(color)
+        }
     }
 
     fun bind(item: Map.Entry<String, Int>) {
+        textCount.text = "%,d".format(item.value)
         textDomain.text = item.key
-        textValue.text = "%,d".format(item.value)
     }
 }

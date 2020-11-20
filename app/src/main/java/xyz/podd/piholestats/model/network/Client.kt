4b85e44dfc176930e44124533a0c2edff1cba850
DenizenB <github@podd.xyz>
2020-11-20 17:57:59 +0100

    `Client` now implements `Parcelable`

--- app/src/main/java/xyz/podd/piholestats/model/network/Client.kt
+++ app/src/main/java/xyz/podd/piholestats/model/network/Client.kt
@@ -1,12 +1,19 @@
 package xyz.podd.piholestats.model.network
 
+import android.os.Parcel
+import android.os.Parcelable
 import kotlinx.serialization.Serializable
 
 @Serializable
-class Client(val name: String, val address: String) {
+class Client(val name: String, val address: String): Parcelable {
     constructor(client: String): this(
         client.substringBefore("|"),
         client.substringAfter("|")
+    )
+
+    constructor(parcel: Parcel): this(
+        parcel.readString()!!,
+        parcel.readString()!!
     )
 
     override fun toString() = when (name.isEmpty()) {
@@ -17,4 +24,21 @@ class Client(val name: String, val address: String) {
     override fun equals(other: Any?) = other is Client && address == other.address
 
     override fun hashCode() = address.hashCode()
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(address)
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR : Parcelable.Creator<Client> {
+        override fun createFromParcel(parcel: Parcel): Client {
+            return Client(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Client?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
